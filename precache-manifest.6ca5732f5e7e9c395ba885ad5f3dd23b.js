c29c33f3db65aea3672e15d540cd731244c1e52f
theomgxz <sg28r8@gmail.com>
2020-06-06 23:06:03 -0400

    Updates

---
+++ precache-manifest.6ca5732f5e7e9c395ba885ad5f3dd23b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01f5360ef7048f733086af81bb9a3248",
+    "url": "/rDomC/index.html"
+  },
+  {
+    "revision": "6db61558008abb1524cf",
+    "url": "/rDomC/static/css/main.573c36f0.chunk.css"
+  },
+  {
+    "revision": "1ca43218603b6dd1e681",
+    "url": "/rDomC/static/js/2.e3b7f7d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rDomC/static/js/2.e3b7f7d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6db61558008abb1524cf",
+    "url": "/rDomC/static/js/main.e7ab9650.chunk.js"
+  },
+  {
+    "revision": "f7477ccf8f562a1a7771",
+    "url": "/rDomC/static/js/runtime-main.6e481703.js"
+  }
+]);
